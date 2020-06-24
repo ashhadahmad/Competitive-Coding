f122355d278103e6f06d35b26bc7d92230e8fd9e
Ashhad Ahmad <ashhadahmed0@gmail.com>
2020-06-24 23:09:42 +0530

    Upsolved Kickstart C problem - Topological Sort

--- GOOGLE/Kickstart C /B.cpp
+++ GOOGLE/Kickstart C /B.cpp
@@ -2,71 +2,66 @@
 using namespace std;
 #define ll long long
 
-void lol(){
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif	
-}
-
-bool dfs(int i, int j, vector<string>& store, vector<vector<bool> >&vis, int r, int c, char cur){
-	if(i == r || j == c || i < 0 || j < 0 || vis[i][j]) return true;
-	if(store[i][j] != cur) return true;
-	vis[i][j] = true;
-	bool a1 = dfs(i+1, j, store, vis,r,c,cur);
-	bool a2 = dfs(i, j+1, store, vis,r,c,cur);
-	bool a3 = dfs(i-1, j, store, vis,r,c,cur);
-	bool a4 = dfs(i, j-1, store, vis,r,c,cur);
-	if(!a1 || !a2 || !a3 || !a4) return false;
-	if(i != r-1) {
-		if(store[i+1][j] != cur && !vis[i+1][j]) return false;
-		return true;
-	}
-	return true;
-}
-
-bool try_solve(int st, int r, int c, vector<string>&store){
-	vector<vector<bool> > vis(r, vector<bool>(c, false));
-	string ans = "";
-	for(int i=r-1; i>=0; i--){
-		for(int j=st; j>=0; j--){
-			if(!vis[i][j]) {
-				ans += store[i][j];
-				bool temp = dfs(i,j,store,vis,r,c, store[i][j]);
-				if(!temp) {
-					return false;
-				}
-			}			
-		}
-		for(int j=st+1; j<c; j++){
-			if(!vis[i][j]) {
-				ans += store[i][j];
-				bool temp = dfs(i,j,store,vis,r,c, store[i][j]);
-				if(!temp) {
-					return false;
-				}
-			}				
-		}
-	}
-	cout << ans << endl;
-	return true;
-}
-
 void solve(){
-	int r, c;
+	int r, c; 
 	cin >> r >> c;
-	vector<string> store(r);
-	for(int i=0; i<r; i++) cin >> store[i];
-	for(int i=0; i<c; i++) {
-		if(try_solve(i,r,c,store))return;
+	vector<string> v(r);
+	for(int i=0; i<r; i++) cin >> v[i];
+	vector<int> vis(26, -1);
+	vector<vector<bool> > after(26, vector<bool>(26, false));
+	vector<int> req(26, 0);
+
+	for(int i=0; i<r; i++) {
+		for(int j=0; j<c; j++) {
+			vis[v[i][j] - 'A'] = 0;
+		}
 	}
-	cout << "-1\n";
-	return;
+
+	for(int i=0; i<r-1; i++){
+		for(int j=0; j<c; j++) {
+			int cur = v[i][j] - 'A';
+			int prev = v[i+1][j] - 'A';
+			if(after[prev][cur] || cur == prev) continue;
+			else {
+				after[prev][cur] = true;
+				req[cur]++;
+			}
+		}
+	}
+
+	queue<int> q;
+	for(int i=0; i<26; i++) {
+		if(req[i] == 0 && vis[i] == 0) q.push(i);
+	}
+
+	vector<char> ans;
+
+	while(!q.empty()) {
+		int best = q.front();
+		q.pop();
+		vis[best] = 1;
+		ans.push_back((char)(best + 'A'));
+		for(int i=0; i<26; i++) {
+			if(after[best][i]) {
+				req[i]--;
+				if(req[i] == 0) q.push(i);
+			}
+		}
+	}
+
+	for(int i=0; i<26; i++) {
+		if(vis[i] == 0) {
+			cout << "-1\n";
+			return;
+		}
+	}
+
+	for(auto x: ans) cout << x;
+	cout << "\n";
+
 }
 
 int main(){
-	// FAST(); 
-	lol();
 	int t; cin >> t;
 	for(int i=1; i<=t; i++) {
 		printf("Case #%d: ",i);
