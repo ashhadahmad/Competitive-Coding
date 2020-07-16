42fa7f43e82a03069187cf3c6d07ba7594668c5e
Ashhad Ahmad <ashhadahmed0@gmail.com>
2020-07-16 19:09:08 +0530

    Upsolved COLE20-D

--- CODECHEF/COLE20/D.cpp
+++ CODECHEF/COLE20/D.cpp
@@ -4,15 +4,9 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define ll long long
 
-struct hold{
-	char c;
-	int x, y;
-	hold(){
-	}
-	hold(char a,int p,int q){
-		c = a; x = p; y = q;
-	}
-};
+bool isvalid(int n, int m, int i, int j){
+	return i < n && i >= 0 && j < m && j >= 0;
+}
 
 void solve(){
 	int n,m;
@@ -20,50 +14,77 @@ void solve(){
 	vector<string> a(n);
 	for(int i=0; i<n; i++) cin >> a[i];
 
-	int worst = -1;
+	vector<vector<bool>> isReachable(n, vector<bool>(m, false));
+	
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+	queue<pair<int, int> > q1;
+	isReachable[n-1][m-1] = true;
+	q1.push(make_pair(n-1,m-1));
+	while(!q1.empty()) {
+		auto cur = q1.front();
+		q1.pop();
+		if(isvalid(n,m,cur.first-1,cur.second) && !visited[cur.first-1][cur.second] && a[cur.first-1][cur.second] != '#'){
+			isReachable[cur.first-1][cur.second] = true;
+			visited[cur.first-1][cur.second] = true;
+			q1.push(make_pair(cur.first-1, cur.second));
+		}
+		if(isvalid(n,m,cur.first,cur.second - 1) && !visited[cur.first][cur.second-1] && a[cur.first][cur.second-1] != '#'){
+			isReachable[cur.first][cur.second-1] = true;
+			visited[cur.first][cur.second-1] = true;
+			q1.push(make_pair(cur.first, cur.second - 1));
+		}
+	}
+
 	for(int i=0; i<n; i++) {
-		if(a[i][m-1] == '#') worst = i;
-	}
-	for(int i=worst; i>=0; i--) {
-		a[i][m-1] = '#';
+		for(int j=0; j<m; j++) visited[i][j] = false;
 	}
 
-	worst = -1;
-	for(int i=0; i<m; i++) {
-		if(a[n-1][i] == '#') worst = i;
-	}
-	for(int i=worst; i>=0; i--) {
-		a[n-1][i] = '#';
-	}
+	// for(int i=0; i<n; i++) {
+	// 	for(int j=0; j<m; j++) {
+	// 		cout << isReachable[i][j] << " "; 
+	// 	}
+	// 	cout << '\n';
+	// }
 
-	queue<hold> q;
-	hold base(a[0][0], 0, 0);
-	q.push(base);
+	queue<pair<int, int> > q;
+	q.push(make_pair(0,0));
 
+	string ans = "";
 	while(!q.empty()) {
 		int size = q.size();
-		cout << q.front().c;
-		set<pair<int, int> > s;
+		auto x = q.front();
+		ans += a[x.first][x.second];
+		vector<pair<int, int> > s;
+		// cout << size << "\n";
 		while(size--) {
-			hold cur = q.front();
+			auto cur = q.front();
 			q.pop();
-			if(cur.x+1 < n) {
-				if(a[cur.x+1][cur.y] != '#') s.insert(make_pair(cur.x+1, cur.y));
+			// printf("Processing %d %d %c\n",cur.first, cur.second, a[cur.first][cur.second]);
+			if(isvalid(n,m,cur.first+1,cur.second) && isReachable[cur.first+1][cur.second] && !visited[cur.first+1][cur.second]){
+				// cout << "Added Down\n";
+				visited[cur.first+1][cur.second] = true;
+				s.push_back(make_pair(cur.first+1,cur.second));
 			}
-			if(cur.y+1 < m) {
-				if(a[cur.x][cur.y+1] != '#') s.insert(make_pair(cur.x, cur.y+1));
+			if(isvalid(n,m,cur.first,cur.second + 1) && isReachable[cur.first][cur.second + 1] && !visited[cur.first][cur.second + 1]){
+				// cout << "Added Right\n";
+				visited[cur.first][cur.second + 1] = true;
+				s.push_back(make_pair(cur.first,cur.second + 1));
 			}
 		}
+		// cout << "Processing Finished for Level\n\n";
+
 		char smallest = '#';
 		for(auto cur: s) {
 			if(smallest == '#' || a[cur.first][cur.second] < smallest) smallest = a[cur.first][cur.second];
 		}
+		// cout << smallest << " "<< s.size() << "\n";
 		for(auto cur: s){
-			if(a[cur.first][cur.second] == smallest) q.push(hold(a[cur.first][cur.second], cur.first, cur.second));
+			if(a[cur.first][cur.second] == smallest) q.push(make_pair(cur.first, cur.second));
 		}
 	}
 
-	cout << "\n";
+	cout << ans << "\n";
 }
 
 int main(){
