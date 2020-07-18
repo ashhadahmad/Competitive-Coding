469157daa0543a6f19a809bc23f1e5f940b68043
Ashhad Ahmad <ashhadahmed0@gmail.com>
2020-07-18 14:06:59 +0530

    Upsolved 1385E

--- CODEFORCES/1385/E.cpp
+++ CODEFORCES/1385/E.cpp
@@ -7,18 +7,57 @@ using namespace std;
 void solve(){
 	int n, m;
 	cin >> n >> m;
-	vector<vector<int>> adjD(n+1);
+	vector<pair<int, int>> store;
 	vector<vector<int>> adjU(n+1);
-	vector<int> pre();
+	vector<vector<int>> pre(n+1);
+	vector<bool> vis(n+1, false);
+
+	vector<int> out(n+1);
 	while(m--) {
 		int type, x, y;
-		if(type) adjD[x].push_back(y);
+		cin >> type >> x >> y;
+		if(type) {
+			store.push_back(make_pair(x,y));
+			out[x]++;
+			pre[y].push_back(x);
+		}
 		else {
 			adjU[x].push_back(y);
 			adjU[y].push_back(x);
 		}
 	}
-	
+
+	queue<int> q;
+	for(int i=1; i<=n; i++) {
+		if(out[i] == 0) q.push(i);
+	}
+
+	while(!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		vis[cur] = true;
+		for(auto x: adjU[cur]){
+			if(!vis[x]) {
+				store.push_back(make_pair(x, cur));
+			}
+		}
+		for(auto x: pre[cur]) {
+			out[x]--;
+			if(out[x] == 0) q.push(x);
+		}
+	}
+
+	for(int i=1; i<=n; i++) {
+		if(out[i] > 0) {
+			cout << "NO\n";
+			return;
+		}
+	}
+
+	cout << "YES\n";
+	for(auto x: store) {
+		cout << x.first << " " << x.second << '\n';
+	}
 }
 
 int main(){
